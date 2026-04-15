c517aba4e93485dc4b9babd3aa5dc4f6470ee9af
LouisL2T <louis26112004@googlemail.com>
2026-04-15 23:18:10 +0200

    Backend fix

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,19 +1,22 @@
 from datetime import datetime, timedelta, timezone
-
 from jose import jwt
-from passlib.context import CryptContext
-
 from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
+import bcrypt
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
+    return hashed_password.decode('utf-8')
 
 def verify_password(plain: str, hashed: str) -> bool:
-    return pwd_context.verify(plain, hashed)
+    try:
+        password_byte_enc = plain.encode('utf-8')
+        hashed_password_byte_enc = hashed.encode('utf-8')
+        return bcrypt.checkpw(password_byte_enc, hashed_password_byte_enc)
+    except ValueError:
+        return False
 
 
 def create_access_token(subject: str, expires_delta: timedelta | None = None) -> str:
