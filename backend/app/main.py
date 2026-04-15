30fe106ddaa6838304cbfd6b4557dda85078bf4b
LouisL2T <louis26112004@googlemail.com>
2026-04-15 23:13:00 +0200

    Backend änderung

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "https://celebrated-baklava-0b1abe.netlify.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
